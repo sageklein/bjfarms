af41e7ebd539d861f3efd81b12469469ad4a5e95
Sage Klein <sageklein@gmail.com>
2021-11-23 21:16:36 -0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import header from "./header1.png";
+import footer from "./footer.png";
+import about from "./santa.jpeg";
+import contact from "./building.jpeg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={header} className="App-logo" alt="Blackjack Farms" />
+			</header>
+			<div className="main-container">
+				<div className="box">
+					<div className="content">
+						<h2>Contact Us</h2>
+						<div id="info">
+							<img
+								src={contact}
+								className="card"
+								alt="Blackjack Farms"
+							/>
+							<p>3331 Hwy 231 N • Shelbyville • TN 37160</p>
+							<p>Phone • 931-437-2573</p>
+							<p>Email • blackjackfarm@charter.net</p>
+							<p>Facebook • facebook.com/blackjackfarm</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="box">
+					<div className="content">
+						<h2>About Us</h2>
+						<div id="info">
+							<img
+								src={about}
+								className="card"
+								alt="Blackjack Farms"
+							/>
+							<p>Family owned buisness</p>
+							<p>All trees only $50 each</p>
+							<p>We have beautiful White Pine Trees</p>
+							<p>
+								Choose and cut down your very own Christmas
+								Tree!
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="footer-container">
+				{" "}
+				<img src={footer} className="footer" alt="Blackjack Farms" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
